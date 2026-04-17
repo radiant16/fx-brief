b5768d4cffb94d6e2c8472c9e89ae4b2458efdfb
radiant16 <radiant164@gmail.com>
2026-04-17 23:20:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE = 'fx-brief-v1';
+const CACHE = 'fx-brief-v260417';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
